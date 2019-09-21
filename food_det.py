ed12db92ace4248f7afce25c7ac2291814fdaebd
Hardik-Chawla <55496082+Hardik-Chawla@users.noreply.github.com>
2019-09-22 04:49:52 +0530

    Add files via upload

--- food_det.py
+++ food_det.py
@@ -1,41 +1,41 @@
+#importing necessary libraries
 import numpy as np
 import cv2
-im=cv2.imread("C:/Users/user/Desktop/r2.jpg")
+# reading image
+im=cv2.imread("C:/Users/user/Desktop/9.jpg")
+# converting image to gray scale
 imgray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+# canning image to remove noises
 imgray = cv2.Canny(imgray, 30, 200)
-#ret,thresh=cv2.threshold(imgray,10,20,cv2.THRESH_BINARY)
+#finding contours in the image
 contours,hierarchy=cv2.findContours(imgray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# now drawing the contours
 cv2.drawContours(im,contours,-1,(255,255,0),3)
 
-# scale_percent = 20 # percent of original size
-# width = int(im.shape[1] * scale_percent / 100)
-# height = int(im.shape[0] * scale_percent / 100)
-# dim = (width, height)
-# resize image
-# resized = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
-
-# #cv2.resize()
-cv2.imshow('frame',im)
+#cv2.imshow('frame',im)
+#initailisng max to zero
 max=0
+# maintaining a counter
 count=0
-for i in range(len(contours)):
-    cnt=contours[i]
+# iterating through the contours
+for itr in range(len(contours)):
+    cnt=contours[itr]
+    #getting area of each contour
     area=cv2.contourArea(cnt)
-    print(area)
+    #print(area)
     if max<area:
         max=area
-thresh=max/4
-for i in range(len(contours)):
-    cnt=contours[i]
+#got the maximum area which is actually the area of plate
+# setting a threshold area
+thresh=max/7
+# calculating the food covered area of plate
+for itr in range(len(contours)):
+    cnt=contours[itr]
     area=cv2.contourArea(cnt)
-    if area>thresh:
+    if area>thresh and area!=max :
         count=count+1
-'''print contours
-print hierarchy'''
-#print(sec_max)
 if count>2:
     print("alert")
 else:
     print("no alert")
-print(max)
 cv2.waitKey(0)
